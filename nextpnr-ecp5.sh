5151d6cc23aadbe7cf50e1d6029c854b5a4a9c41
Zachary Yedidia <zyedidia@gmail.com>
2022-01-14 16:36:00 -0800

    Update for nextpnr

--- nextpnr-ecp5.sh
+++ nextpnr-ecp5.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/YosysHQ/nextpnr nextpnr
-cd nextpnr
+git clone https://github.com/YosysHQ/nextpnr nextpnr-ecp5
+cd nextpnr-ecp5
 cmake -DARCH=ecp5 -DCMAKE_INSTALL_PREFIX=/usr/local .
 make
 sudo make install
